be3e8dd2fa24f63430bcb162289a16f44180edb0
Zeev Rotshtein <zeev.rotshtein@cellebrite.com>
2016-08-02 16:23:38 +0300

    Initial commit

---
+++ google_calendar.py
@@ -0,0 +1,158 @@
+# TODO: Figure out correct authentication scheme for servers
+# TODO: Web interface
+
+from __future__ import print_function
+
+import httplib2
+import os
+import dateutil
+import ical
+
+import json
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+from constants import APPLICATION_NAME, CLIENT_SECRET_FILE, MY_CALENDAR_ID, MY_ICS_URL
+
+
+def j(o, pretty=False):
+    if pretty:
+        print(json.dumps(o, separators=(',', ': '), indent=2, sort_keys=True))
+    else:
+        print(json.dumps(o, separators=(',', ':'), sort_keys=True))
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    credential_dir = os.path.join(os.path.expanduser('~'), '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'calendar-python-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, 'https://www.googleapis.com/auth/calendar')
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def translate_dt(s):
+    return dateutil.parser.parse(s).strftime('%Y-%m-%dT%H:%M:%S%z')
+
+
+def get_entire_list(func, **kw):
+    page_token = None
+    ret = []
+    while True:
+        page = func(pageToken=page_token, **kw).execute()
+        ret += page['items']
+        page_token = page.get('nextPageToken')
+        if not page_token:
+            break
+    return ret
+
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    if 0:
+        calendars = get_entire_list(service.calendarList().list, minAccessRole='writer')
+        for calendar in calendars:
+            print('%-20s [%-52s] primary=%5s  selected=%5s  description=%s' % (calendar['summary'],
+                                                                               calendar['id'],
+                                                                               calendar.get('primary', False),
+                                                                               calendar.get('selected', False),
+                                                                               calendar.get('description', '')))
+        return
+
+    if 0:
+        try:
+            service.calendars().delete(calendarId=MY_CALENDAR_ID).execute()
+        except discovery.HttpError as e:
+            if e.resp['status'] != '404':
+                raise
+        primary_calendar = service.calendars().get(calendarId='primary').execute()
+        j(service.calendars().insert(body={'summary': 'Facebook', 'timeZone': primary_calendar['timeZone']}).execute(), True)
+        return
+
+    all_events = {e['id']: e for e in get_entire_list(service.events().list, calendarId=MY_CALENDAR_ID)}
+
+    if 0:
+        j(all_events.values())
+        return
+
+    if 0:
+        j({event['id']: event['updated'] for event in all_events.values()}, True)
+        return
+
+    src_calendars = ical.get_url_tree(MY_ICS_URL)
+    events_to_delete = set(all_events.keys())
+    for src_calendar in src_calendars:
+        for src_event in src_calendar['_items']:
+            event_id = src_event['UID'][:src_event['UID'].find('@')]
+
+            print('--->', event_id, src_event['SUMMARY'])
+
+            dst_event = all_events.get(event_id)
+            if dst_event:
+                events_to_delete.remove(event_id)
+                if 'LAST-MODIFIED' in src_event and dateutil.parser.parse(src_event['LAST-MODIFIED']) <= dateutil.parser.parse(dst_event['updated']):
+                    print('Skip')
+                    continue
+
+            event = {'id': event_id,
+                     'summary': src_event['SUMMARY'],
+                     'start': {'dateTime': translate_dt(src_event['DTSTART'])},
+                     'source': {'title': 'Facebook event',
+                                'url': src_event['URL']}}
+
+            if 'DTEND' in src_event:
+                event['end'] = {'dateTime': translate_dt(src_event['DTEND'])}
+            if 'DESCRIPTION' in src_event:
+                event['description'] = src_event['DESCRIPTION']
+            if 'LOCATION' in src_event:
+                event['location'] = src_event['LOCATION']
+
+            j(event)
+
+            try:
+                if dst_event is None:
+                    print('insert')
+                    event = service.events().insert(calendarId=MY_CALENDAR_ID, body=event).execute()
+                else:
+                    print('update')
+                    event = service.events().update(calendarId=MY_CALENDAR_ID, eventId=event_id, body=event).execute()
+            except discovery.HttpError as e:
+                print('Error!', e)
+
+    for event_id in events_to_delete:
+        print('Delete', event_id)
+        service.events().delete(calendarId=MY_CALENDAR_ID, eventId=event_id).execute()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ical.py
@@ -0,0 +1,144 @@
+﻿from __future__ import unicode_literals
+
+import urllib
+import json
+
+from dateutil import parser
+
+
+from constants import MY_ICS_URL
+
+
+def j(o, pretty=False):
+    if pretty:
+        print(json.dumps(o, separators=(',', ': '), indent=2, sort_keys=True))
+    else:
+        print(json.dumps(o, separators=(',', ':')))
+
+
+def unescape_str(s):
+    D = {'\\': '\\',
+         "'": "'",
+         '"': '"',
+         'b': '\b',
+         'f': '\f',
+         't': '\t',
+         'n': '\n',
+         'r': '\r',
+         'v': '\v',
+         'a': '\a'}
+
+    ret = ''
+    i = 0
+    while True:
+        n = s.find('\\', i)
+        if n == -1 or n == len(s) - 1:
+            ret += s[i:]
+            break
+
+        ret += s[i: n]
+        i = n + 1
+        c = s[i]
+
+        if c in D:
+            ret += D[c]
+            i += 1
+        elif c == 'x':
+            ret += unichr(int(s[i + 1: i + 3], 16))
+            i += 3
+        elif c.isdigit():
+            ret += unichr(int(s[i: i + 3], 8))
+            i += 3
+        else:
+            ret += c
+            i += 1
+
+    return ret
+
+
+def get_file_tree(file_obj):
+    ret = []
+    stack = []
+    obj = None
+    lines = iter(file_obj)
+    whole_line = lines.next()
+    while True:
+        # TODO: Support UTF-8 decoding with line-breaks mid-multibyte characters
+        try:
+            line = lines.next().decode('utf8').strip('\r\n')
+            if line[0] in ' \t':
+                whole_line += line[1:]
+                continue
+        except StopIteration:
+            line = None
+
+        try:
+            field, value = whole_line.split(':', 1)
+            value = unescape_str(value)
+
+            if field == 'BEGIN':
+                if obj is not None:
+                    stack.append(obj)
+                obj = {'_type': value}
+                continue
+
+            if obj is None:
+                obj = stack.pop()
+
+            if field == 'END':
+                if stack:
+                    stack[-1].setdefault('_items', []).append(obj)
+                else:
+                    ret.append(obj)
+                obj = None
+                continue
+
+            if field:
+                if ';' in field:
+                    # TODO: Support quoted string values
+                    # TODO: Support multiple values (comma-separated)
+                    values = field.split(';')
+                    field = values.pop(0)
+                    values = dict(v.split('=') for v in values)
+                    values['_value'] = value
+                    obj.setdefault(field, []).append(values)
+                else:
+                    obj[field] = value
+        finally:
+            if line is None:
+                break
+            whole_line = line
+
+    return ret
+
+
+def get_url_tree(url):
+    return get_file_tree(urllib.urlopen(url))
+
+
+def test():
+    tree = get_url_tree(MY_ICS_URL)
+
+    if 0:
+        j(tree, True)
+        return
+
+    if 0:
+        j({event['UID'][:event['UID'].find('@')]:
+           parser.parse(event['LAST-MODIFIED']).strftime('%Y-%m-%dT%H:%M:%S%z')
+           for event in tree[0]['_items']}, True)
+        return
+
+    for calendar in tree:
+        for event in reversed(calendar['_items']):
+            print '<%s> [%-12s] {%8s} %s by %s' % (
+                parser.parse(event['DTSTART']).strftime('%Y-%m-%d %H:%M'),
+                (event.get('PARTSTAT', '').replace('-', ' ').capitalize() or 'N/A').center(12),
+                event.get('SEQUENCE', '   N/A  '),
+                event['SUMMARY'],
+                event.get('ORGANIZER', [{'CN': 'Facebook'}])[0]['CN'],
+            )
+
+
+if __name__ == '__main__':
+    test()
